e831baff284bafa2ea03ab7c1edc6f4b9d136be6
Volodymyr Kulikovskyi <kulikovskyi@hotmail.com>
2021-03-16 22:36:27 +0200

    Added divide and conquer implementation

--- AlgoWorkout/AlgoWorkout.Tests/Cormen/MaxSubarrayTests.cs
+++ AlgoWorkout/AlgoWorkout.Tests/Cormen/MaxSubarrayTests.cs
@@ -1,12 +1,28 @@
 using AlgoWorkout.Cormen;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace AlgoWorkout.Tests.Cormen
 {
     [TestClass]
     public class MaxSubarrayTests
     {
+        private const int LongArraySize = 20_000;
         public static MaxSubarray sut = new MaxSubarray();
+        public static int[] LongArray = new int[LongArraySize];
+
+        [ClassInitialize]
+        public static void Init(TestContext context) 
+        {
+            Random rand = new Random(13);
+            int initialPrice = 100;
+            LongArray[0] = initialPrice;
+            for (int i = 1; i < LongArraySize; i++)
+            {
+                int delta = rand.Next(-50, 50);
+                LongArray[i] = LongArray[i - 1] + delta;
+            }
+        }
 
         [TestMethod]
         public void TestNaiveImplementation()
@@ -20,12 +36,54 @@ namespace AlgoWorkout.Tests.Cormen
         }
 
         [TestMethod]
-        public void TestNaiveImplementationLong()
+        public void TestNaiveImplementationStockArray()
         {
             var input = new int[] { 100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97 };
             int maxOutcome = 106 - 63;
 
             int result = sut.BruteForce(input);
+
+            Assert.AreEqual(maxOutcome, result);
+        }
+
+        [TestMethod]
+        public void TestNaiveImplementationOnLongArray()
+        {
+            var input = LongArray;
+            
+            int result = sut.BruteForce(input);
+
+            Assert.AreEqual(4621, result);
+        }
+
+        [TestMethod]
+        public void TestDivideAndConquerImplementation()
+        {
+            var input = new int[] { 10, 11, 7, 10, 6 };
+            int maxOutcome = 3;
+
+            int result = sut.DivideAndConquer(input);
+
+            Assert.AreEqual(maxOutcome, result);
+        }
+
+        [TestMethod]
+        public void TestDivideAndConquerImplementationOnLongArray()
+        {
+            var input = LongArray;
+
+            int result = sut.DivideAndConquer(input);
+
+            Assert.AreEqual(4621, result);
+        }
+
+        [TestMethod]
+        public void TestDivideAndConquerImplementationStockArray()
+        {
+            var input = new int[] { 100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97 };
+            int maxOutcome = 106 - 63;
+
+            int result = sut.DivideAndConquer(input);
 
             Assert.AreEqual(maxOutcome, result);
         }
--- AlgoWorkout/AlgoWorkout/Cormen/MaxSubarray.cs
+++ AlgoWorkout/AlgoWorkout/Cormen/MaxSubarray.cs
@@ -1,17 +1,10 @@
-﻿using System;
-
-namespace AlgoWorkout.Cormen
+﻿namespace AlgoWorkout.Cormen
 {
     public class MaxSubarray
     {
         public int BruteForce(int[] input) 
         {
-            int[] diff = new int[input.Length];
-
-            for (int i = 1; i< input.Length; i++)
-            {
-                diff[i] = input[i] - input[i - 1];
-            }
+            var diff = GetDifferencesArray(input);
 
             int max = int.MinValue;
             for (int i = 0; i< diff.Length; i++)
@@ -29,7 +22,70 @@ namespace AlgoWorkout.Cormen
 
         public int DivideAndConquer(int[] input)
         {
-            return 0;
+            return FindMaxArray(GetDifferencesArray(input), 0, input.Length - 1).sum;
+        }
+
+        private (int leftBound, int rightBound, int sum) FindMaxArray(int[] array, int leftBound, int rightBound)
+        {
+            if (leftBound == rightBound)
+            {
+                return (leftBound, rightBound, array[rightBound]);
+            }
+            
+            var mid = (rightBound - leftBound) / 2 + leftBound;
+            var leftRes = FindMaxArray(array, leftBound, mid);
+            var rightRes = FindMaxArray(array, mid + 1, rightBound);
+
+            var midRes = FindCrossingSum(array, leftBound, rightBound, mid);
+
+            if (leftRes.sum >= rightRes.sum && leftRes.sum >= midRes.sum)
+                return (leftBound: leftRes.leftBound, rightBound: leftRes.rightBound, sum: leftRes.sum);
+            else if (leftRes.sum <= rightRes.sum && rightRes.sum >= midRes.sum)
+                return (leftBound: rightRes.leftBound, rightBound: rightRes.rightBound, sum: rightRes.sum);
+            else 
+                return (leftBound: midRes.leftIndex, rightBound: midRes.rightIndex, sum: midRes.sum);
+        }
+
+        private (int leftIndex, int rightIndex, int sum) FindCrossingSum(int[] array, int leftIndex, int rightIndex, int midIndex) 
+        {
+            int leftSum = int.MinValue;
+            int sum = 0;
+            int leftMinIndex = midIndex;
+            for (int i = midIndex; i >= leftIndex; i--)
+            {
+                sum += array[i];
+                if (leftSum <= sum)
+                {
+                    leftSum = sum;
+                    leftMinIndex = i;
+                }
+            }
+
+            int rightSum = int.MinValue;
+            int rightMaxIndex = midIndex;
+            sum = 0;
+            for (int i = midIndex + 1; i <= rightIndex; i++)
+            {
+                sum += array[i];
+                if (rightSum <= sum)
+                {
+                    rightSum = sum;
+                    rightMaxIndex = i;
+                }
+            }
+
+            return (leftMinIndex, rightMaxIndex, leftSum + rightSum);
+        }
+
+        private int[] GetDifferencesArray(int[] input)
+        {
+            int[] diff = new int[input.Length];
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                diff[i] = input[i] - input[i - 1];
+            }
+            return diff;
         }
     }
 }
