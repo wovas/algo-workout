2469e42db87b39a357658cb507d7c04dbe77996f
Volodymyr Kulikovskyi <kulikovskyi@hotmail.com>
2021-03-21 16:04:12 +0200

    Added binary search implementation

---
+++ AlgoWorkout/AlgoWorkout.Tests/Educative/BinarySearchTests.cs
@@ -0,0 +1,30 @@
+﻿using AlgoWorkout.Educative;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoWorkout.Tests.Educative
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        [TestMethod]
+        public void FindExisting()
+        {
+            var arr = new int[] { 1, 3, 5, 6, 7, 8, 9, 10, 11, 13 };
+            Assert.AreEqual(0, BinarySearch.Search(arr, 1));
+            Assert.AreEqual(2, BinarySearch.Search(arr, 5));
+            Assert.AreEqual(9, BinarySearch.Search(arr, 13));
+        }
+
+        [TestMethod]
+        public void NotFoundAbsent()
+        {
+            var arr = new int[] { 1, 3, 5, 6, 7, 8, 9, 10, 11, 13 };
+            Assert.AreEqual(-1, BinarySearch.Search(arr, 2));
+            Assert.AreEqual(-1, BinarySearch.Search(arr, 12));
+            Assert.AreEqual(-1, BinarySearch.Search(arr, 16));
+        }
+    }
+}
---
+++ AlgoWorkout/AlgoWorkout/Educative/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgoWorkout.Educative
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] arr, int target)
+        {
+            return Search(arr, target, 0, arr.Length - 1);
+        }
+
+        private static int Search(int[] arr, int target, int left, int right)
+        {
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (arr[mid] > target)
+                {
+                    return Search(arr, target, left, mid - 1);
+                }
+                else if (arr[mid] < target)
+                {
+                    return Search(arr, target, mid + 1, right);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
